076c4ced9a85bba7e734695c00364e87c285412a
niranj775 <niranj775@gmail.com>
2021-07-13 12:42:00 +0530

    13-07-2021

--- script.js
+++ script.js
@@ -1,60 +1,51 @@
-var xhr = new XMLHttpRequest(); //Creating the XMLHTTPRequest
+var xhr = new XMLHttpRequest();
 
 xhr.open(
   "GET",
   "https://raw.githubusercontent.com/Rajavasanthan/jsondata/master/pagenation.json",
   true
-); //Opening the XMLHTTPRequest
+);
 
-xhr.send(); //Sending the XMLHTTPRequest
+xhr.send();
 
 xhr.onload = function () {
-  var res = JSON.parse(this.response); //Saving the response from the API in res variable
+  var res = JSON.parse(this.response);
 
-  var head3 = document.createElement("h3"); // Creating the H3 tag using DOM
-  head3.innerHTML = "DOM PAGINATION"; // Adding the contenet to the created H3 tag
-  document.body.append(head3); // Appending the H3 Element to the body
+  var head3 = document.createElement("h3");
+  head3.innerHTML = "DOM PAGINATION";
+  document.body.append(head3);
 
-  var table = document.createElement("table"); // Creating the table using DOM
-  table.setAttribute("class", "table table-bordered"); // Adding class to the table
+  var table = document.createElement("table");
+  table.setAttribute("class", "table table-bordered");
 
-  var thead = document.createElement("thead"); // Creating the thead using DOM
-  thead.setAttribute("class", "thead"); // Adding class to the thead
+  var thead = document.createElement("thead");
+  thead.setAttribute("class", "thead");
 
-  var tr = document.createElement("tr"); // Creating the trow using DOM
+  var tr = document.createElement("tr");
 
-  var th0 = createThTd("th", "ID"); // Creating th1 using DOM
-  var th1 = createThTd("th", "Name"); // Creating th2 using DOM
-  var th2 = createThTd("th", "Email"); // Creating th3 using DOM
+  var th0 = createThTd("th", "ID");
+  var th1 = createThTd("th", "Name");
+  var th2 = createThTd("th", "Email");
 
-  tr.append(th0, th1, th2); // Appending the ths to trow
-  thead.append(tr); // Appending the trow to thead
+  tr.append(th0, th1, th2);
+  thead.append(tr);
 
-  var tbody = document.createElement("tbody"); // Creating the tbody using DOM
+  var tbody = document.createElement("tbody");
 
-  // Loop to create tr, th, tds and append it to the table
-  let currentPage = 1;
+  let currentPage = 0;
   let pageSize = 10;
-  if (currentPage == 1) {
-    for (let i = 0; i < res.length / pageSize; i++) {
-      var tr = document.createElement("tr");
-      var th = createThTd("th", res[i].id);
-      var td1 = createThTd("td", res[i].name);
-      var td2 = createThTd("td", res[i].email);
-      tr.append(th, td1, td2);
-      tbody.append(tr);
-    }
+  if (currentPage == 0) {
+    pageload("0");
   }
 
-  // Function to create the element
   function createThTd(ele, value) {
     var ele = document.createElement(ele);
     ele.innerHTML = value;
     return ele;
   }
 
-  table.append(thead, tbody); // Appending the thead, tbody to table
-  document.body.append(table); // Appending table to document body
+  table.append(thead, tbody);
+  document.body.append(table);
 
   var nav = document.createElement("nav");
   nav.setAttribute("aria-label", "Page navigation example");
@@ -79,7 +70,7 @@ xhr.onload = function () {
 
     var a = document.createElement("a");
     a.setAttribute("class", "page-link");
-    a.addEventListener("click", display);
+    a.addEventListener("click", (e) => display(i));
     a.innerHTML = i + 1;
 
     li.append(a);
@@ -90,7 +81,7 @@ xhr.onload = function () {
 
   var anex = document.createElement("a");
   anex.setAttribute("class", "page-link");
-  anex.setAttribute("click", displayNext);
+  anex.addEventListener("click", displayNext);
   anex.innerHTML = "Next";
 
   linex.append(anex);
@@ -99,7 +90,32 @@ xhr.onload = function () {
 
   document.body.append(nav);
 
-  function display() {}
-  function displayPrev() {}
-  function displayNext() {}
+  function display(selectedPage) {
+    currentPage = selectedPage;
+
+    pageload(currentPage);
+  }
+  function displayPrev() {
+    if (currentPage >= 0) {
+      currentPage -= 1;
+      pageload(currentPage);
+    }
+  }
+  function displayNext() {
+    if (currentPage < 10) {
+      currentPage += 1;
+      pageload(currentPage);
+    }
+  }
+
+  function pageload(x) {
+    for (let i = x * 10; i < x * 10 + res.length / pageSize; i++) {
+      var tr = document.createElement("tr");
+      var th = createThTd("th", res[i].id);
+      var td1 = createThTd("td", res[i].name);
+      var td2 = createThTd("td", res[i].email);
+      tr.append(th, td1, td2);
+      tbody.append(tr);
+    }
+  }
 };
